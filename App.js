21b1b03a468269bad6e9714776b5e1c5a5db3263
TalentDev <talentdev@TalentDevs-iMac-Pro.local>
2018-06-27 19:36:31 +0300

    Updated

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
+import { Container, Content, Header, Body, StyleProvider, Text, Card, CardItem, Thumbnail} from 'native-base';
+import getSampleTheme from './src/theme';
 
 export default class App extends React.Component {
-  render() {
+
+    render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <StyleProvider style={getSampleTheme()}>
+            <Container>
+                <Header>
+                    <Body><Text title>Native Base Code Sample</Text></Body>
+                </Header>
+                <Content>
+                    <Card>
+                        <CardItem cardBody>
+                            <Image style={{ height: 250, width: null, flex: 1}} source={require('./src/assets/1.png')} />
+                        </CardItem>
+                        <CardItem>
+                            <Thumbnail xSmall source={require('./src/assets/user.jpg')}/>>
+                            <Text comment>How about this photo?</Text>
+                        </CardItem>
+                    </Card>
+                </Content>
+            </Container>
+        </StyleProvider>    
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
